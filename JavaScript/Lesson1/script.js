37106a9383f9d7adddaaffc6531aabf0cc1f2d37
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-10-30 17:29:39 +0400

    Upload Hw1

--- JavaScript/Lesson1/script.js
+++ JavaScript/Lesson1/script.js
@@ -1,10 +1,9 @@
-
-
+//Задание 1//
 let Tc = prompt("Введите температуру по цельсию");
 let Tf = (9 / 5) * Tc + 32
 alert(Tf)
 
-
+//задание 2//
 let name = "Васиилий";
 let admin = name;
 alert(admin)
